e3694b0264a8d6030b16530df3b1df48c7af34c1
Cheney <sollenberger.15@wright.edu>
2019-08-06 15:25:17 -0400

    Commenting and reformatting.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::thread;
-use std::time::Duration;
 fn main() {
     /*
     let correct = 
@@ -58,9 +56,7 @@ fn main() {
     [0,0,0,0,0,9,7,0,0].to_vec()].to_vec();
   */
     let solved = sudoku_solver(&input);
-    println!("Solved:");
     print(&solved);
-   // assert_eq!(solved, correct);
 }
 
 fn sudoku_solver(sudoku: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
@@ -69,6 +65,7 @@ fn sudoku_solver(sudoku: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     game
 }
 
+/* Recursively looks for and inserts valid guesses until puzzle is solved. */
 fn recurse(mut v: &mut std::vec::Vec<std::vec::Vec<i32>>) -> bool {
     match find_zero(&v) {
         Some((r, c)) => {
@@ -89,49 +86,44 @@ fn recurse(mut v: &mut std::vec::Vec<std::vec::Vec<i32>>) -> bool {
     return false;
 }
 /*
+/* Finds possible guesses given the board and coordinate. */
 fn get_guesses(r:i32, c:i32, board: &Vec<Vec<i32>>) -> Vec<i32> {
     let mut result = vec!();
     let column: Vec<i32> = board.iter().map(|x| x[c as usize]).collect();
     let subgrid = get_subgrid(r, c, board);
     
     for guess in 1..10 {
-       // println!("Col: {:?}, Row: {:?}, Subgrid: {:?}, Guess: {}", column, board[r as usize], subgrid, guess);
        // Check the row, column and subgrid if the item does not exist.
         if !board[r as usize].contains(&guess) && !column.contains(&guess) && !subgrid.contains(&guess) {
             result.push(guess);   
         } 
     }
-    //println!("Col: {:?}, Row: {:?}, Subgrid: {:?}, Res: {:?}", column, board[r as usize], subgrid, result);
     return result
 }
 */
 
+/* Checks if a given num is valid at the given coordinate in the given board. */
 fn valid(board: &mut std::vec::Vec<std::vec::Vec<i32>>, r: i32, c: i32, num: i32) -> bool {
     let mut bo = board.clone();
     bo[r as usize][c as usize] = num;
     // Check row for duplicates.
-    //println!("Row: {:?}", bo[r as usize]);
     if contains_duplicates(&bo[r as usize]) {
-        //println!("Found Duplicate.");
         return false
     }
     // Check column for duplicates.
     let column: Vec<i32> = bo.iter().map(|x| x[c as usize]).collect();
-   // println!("Col: {:?}", column);
     if contains_duplicates(&column) {
-        //println!("Found Duplicate.");
         return false
     }
     // Check subgrid for duplicates
     let subgrid = get_subgrid(r, c, &bo);
-    //       println!("Subgrid: {:?}", subgrid);
     if contains_duplicates(&subgrid) {
-    //    println!("Found Duplicate.");
         return false
     }
     return true
 }
 
+/* Traverses the given board and finds the first unpopulated (0) value. */
 fn find_zero(v: &Vec<Vec<i32>>) -> Option<(i32, i32)> {
     for r in 0..9 {
         for c in 0..9 { 
@@ -143,23 +135,22 @@ fn find_zero(v: &Vec<Vec<i32>>) -> Option<(i32, i32)> {
     return None
 }
 
+/* Given a vector, checks if any duplicates exist. */
 fn contains_duplicates(v: &Vec<i32>) -> bool{
     let mut track: Vec<i32> = [0;9].to_vec();
     for i in 0..9{
         if v[i as usize] > 0 {
-           // println!("{:?}", v[i as usize]);
             track[(v[i as usize] - 1) as usize] =  track[(v[i as usize] - 1) as usize] + 1;
         }
     }
-   // println!("Track: {:?}",track);
     let violations: Vec<i32> = track.into_iter().filter(|&x| x > 1).collect();
-  //  println!("Violators:{:?}", violations);
     if violations.len() > 0 {
         return true
     }
     return false    
 }
 
+/* Given the board and a coordinate, returns the correct subgrid in a vector. */
 fn get_subgrid(r: i32, c:i32, board: &Vec<Vec<i32>>) -> Vec<i32> {
     let grid_col = c/3;
     let grid_row = r/3;
@@ -172,6 +163,7 @@ fn get_subgrid(r: i32, c:i32, board: &Vec<Vec<i32>>) -> Vec<i32> {
     subgrid
 }
 
+/* Prints the board in a semi-formatted manner. */
 fn print(v: &Vec<Vec<i32>>) {
     for i in 0..9 {
         println!("{:?}", v[i]);
